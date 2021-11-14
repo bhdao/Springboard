989ec21e1c62c1b22c6774c72b43ed50129ac65e
brendan <onthedaolow@gmail.com>
2021-11-13 21:13:27 -0800

    Added 10.6 Destructuring exercise

---
+++ 10/10.6 Destructuring/app.js
@@ -0,0 +1,15 @@
+const obj = {
+  numbers: { a: 1, b: 2 }
+};
+
+let { a, b } = obj.numbers;
+
+
+///////////////////////////
+
+let arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+///////////////////////////
+
+const raceResults = ([first, second, third, ...rest]) => { return { first, second, third, rest: [rest] } };
